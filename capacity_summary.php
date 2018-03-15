2abeee65367982b4a363bb8c021c9ae67b46cad5
rlilli07 <rlilli07@gmail.com>
2018-03-15 10:42:59 -0500

    Added capacity_summary.php page
    Added the capacity_summary.php page. The database is pulling in all the data from the capacity table. The summary icon is working with a check mark next to it.

--- capacity_summary.php
+++ capacity_summary.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION)) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Capacity - Cadence</title>
+	<title>Capacity - Summary</title>
     
 	<!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -51,47 +51,54 @@ if (!isset($_SESSION)) {
 				<nav class="nav-left">
 					<ul class="nav nav-stacked">
 						<li><a href="capacity_activePI.php"><img src="./icons/capacity_active_pi.png" style="width:40px;height:50px;">Active PI</a></li>
-						<li><a href="capacity_cadence.php"><img style="width:40px;height:50px;" src="./icons/capacity_cadence.png" /><img src="./icons/image15.png" style="width:20px;height:30px;">Cadence</a></li>
-						<li><a href="#"><img class="icon" src="./icons/capacity_calculate.png" />Calculate</a></li>
-						<li><a href="capacity_summary.php"><img class="icon" src="./icons/capacity_summary.png" />Summary</a></li>
+						<li><a href="capacity_cadence.php"><img style="width:40px;height:50px;" src="./icons/capacity_cadence.png" />Cadence</a></li>
+						<li><a href=""><img class="icon" src="./icons/capacity_calculate.png" />Calculate</a></li>
+						<li><a href="capacity_summary.php"><img src="./icons/capacity_summary.png" style="width:40px;height:50px;"/><img src="./icons/image15.png" style="width:20px;height:30px;">Summary</a></li>
 						<li><a href="#"><img class="icon" src="./icons/capacity_trend.png" />Trend</a></li>
 					</ul>
 				</nav>
 			</div>
 			<div class="col-md-10">
 			<hr>
-			<h3><font size="4" color="blue">Cadence:</font></h3>
+			<h3><font size="4" color="blue">Summary:</font></h3>
 			</hr>	
 				<table style="font-family:arial;" id="info" cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered"
 					   width="100%">
 
 						<tr>
-							<th>Sequence</th>
+							<th>id</th>
+							<th>Team ID</th>
+							<th>Team Name</th>
 							<th>Program Increment</th>
-							<th>Iteration</th>
-							<th>Start Date</th>
-							<th>End Date</th>
-							<th>Duration</th>
-							<th>Notes</th>
+							<th>Iteration 1</th>
+							<th>Iteration 2</th>
+							<th>Iteration 3</th>
+							<th>Iteration 4</th>
+							<th>Iteration 5</th>
+							<th>Iteration 6</th>
+							<th>Total</th>
 						</tr>
 					<?php
 						require 'db_configuration.php';
 						
-						$sql = "SELECT * FROM trains_and_teams LIMIT 3";
+						$sql = "SELECT * FROM capacity LIMIT 3";
 						$result = run_sql($sql);
 						
 						// output data of each
 						if ($result->num_rows > 0) {
 							while ($row = $result->fetch_assoc()) {
 								echo '<tr>
-									<td>' . $row["team_id"] . "</td>
-									<td>" . $row["type"] . "</td>
-									<td>" . $row["name"] . "</td>
-									<td>" . $row["name"] . "</td>
-									<td>" . $row["name"] . "</td>
-									<td>" . $row["name"] . "</td>
-									<td>" . $row["name"] . "</td>
-									<td>" . $row["name"] . "</td>
+									<td>' . $row["id"] . "</td>
+									<td>" . $row["team_id"] . "</td>
+									<td>" . $row["team_name"] . "</td>
+									<td>" . $row["program_increment"] . "</td>
+									<td>" . $row["iteration_1"] . "</td>
+									<td>" . $row["iteration_2"] . "</td>
+									<td>" . $row["iteration_3"] . "</td>
+									<td>" . $row["iteration_4"] . "</td>
+									<td>" . $row["iteration_5"] . "</td>
+									<td>" . $row["iteration_6"] . "</td>
+									<td>" . $row["total"] . "</td>
 								</tr>";
 						}
 					} else {
