9a0f429f88e294e06a38f199920a664116168485
pcwer <david.xuong.tran@gmail.com>
2018-04-03 02:09:18 -0500

    Added partial view.php functionality for EMP and AT

--- training_summary.php
+++ training_summary.php
@@ -4,7 +4,8 @@
 	 <?PHP
     session_start();
     require('session_validation.php');
-	require('db_configuration.php')
+	require('db_configuration.php');
+	require('view.php');
     ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,264 +45,40 @@
 	</div>
 	
 	<!-- Primary content goes here -->
-	<?php
-		function displayValues(&$array) {
-			$array = array_unique($array);
-			
-			foreach($array as $v) {
-				echo $v."<br>";
-			}
-		}
-	?>
-	<?php
-		$empNbr = $firstName = $lastName = $emailAddress = $city = $country = $managerName = $employeeType = '';
-		$agileTeamName = $agileReleaseTrainName = $solutionTrainName = '';
-		$role = $status = $courseName = $courseCode = $trainer = $dates = array();
-		$type = $id = $rows = '';
-		
-		if (isset($_GET['type'])) {
-			$type = $_GET['type'];
-		}
-		
-		// Employees query
-		if (count($_GET) == 0) {
-			$sql = "SELECT e.employee_nbr,
-				e.first_name, 
-				e.last_name, 
-				e.email_address, 
-				e.city, 
-				e.country, 
-				CONCAT(m.first_name, ' ', m.last_name) AS manager_name,
-				mt.role,
-				mt.at_name,
-				mt.art_name,
-				mt.st_name,
-				tec.status,
-				tec.course_name,
-				tec.course_code,
-				tec.trainer,
-				tec.dates
-			FROM employees e
-			LEFT OUTER JOIN employees m ON e.managers_nbr = m.employee_nbr
-			LEFT OUTER JOIN (
-				SELECT m.employee_nbr,
-					m.role, 
-					tt_at.name AS at_name, 
-					tt_art.name AS art_name, 
-					tt_st.name AS st_name
-				FROM trains_and_teams tt_at
-				JOIN trains_and_teams tt_art ON tt_at.parent = tt_art.team_id
-				JOIN trains_and_teams tt_st ON tt_art.parent = tt_st.team_id
-				LEFT OUTER JOIN membership m ON tt_at.team_id = m.team_id) mt
-				ON e.employee_nbr = mt.employee_nbr
-			LEFT OUTER JOIN (
-				SELECT te.first_name, 
-					te.last_name,
-					te.email AS email_address,
-					tc.status, 
-					tc.course_name, 
-					tc.course_code, 
-					CONCAT(tc.trainer_first_name, ' ', tc.trainer_last_name) AS trainer,
-					CONCAT(DATE_FORMAT(tc.start_date, '%m/%d/%Y'), ' - ', DATE_FORMAT(tc.end_date, '%m/%d/%Y')) AS dates
-				FROM training_calendar tc
-				JOIN training_enrollment te ON tc.training_id = te.training_id) tec
-				ON (e.first_name = tec.first_name
-					AND e.last_name = tec.last_name
-					AND e.email_address = tec.email_address
-					)
-				LIMIT 1
-				";
-		} else {
-			if (isset($_GET["id"])) {
-				$id = $_GET["id"];
-			}
-
-			if(isset($_GET["type"])){
-				$employeeType = $_GET["type"];
-			}
-			
-			$sql = "SELECT e.employee_nbr,
-				e.first_name, 
-				e.last_name, 
-				e.email_address, 
-				e.city, 
-				e.country, 
-				CONCAT(m.first_name, ' ', m.last_name) AS manager_name,
-				mt.role,
-				mt.at_name,
-				mt.art_name,
-				mt.st_name,
-				tec.status,
-				tec.course_name,
-				tec.course_code,
-				tec.trainer,
-				tec.dates
-			FROM employees e
-			LEFT OUTER JOIN employees m ON e.managers_nbr = m.employee_nbr
-			LEFT OUTER JOIN (
-				SELECT m.employee_nbr,
-					m.role, 
-					tt_at.name AS at_name, 
-					tt_art.name AS art_name, 
-					tt_st.name AS st_name
-				FROM trains_and_teams tt_at
-				JOIN trains_and_teams tt_art ON tt_at.parent = tt_art.team_id
-				JOIN trains_and_teams tt_st ON tt_art.parent = tt_st.team_id
-				LEFT OUTER JOIN membership m ON tt_at.team_id = m.team_id) mt
-				ON e.employee_nbr = mt.employee_nbr
-			LEFT OUTER JOIN (
-				SELECT te.first_name, 
-					te.last_name,
-					te.email AS email_address,
-					tc.status, 
-					tc.course_name, 
-					tc.course_code, 
-					CONCAT(tc.trainer_first_name, ' ', tc.trainer_last_name) AS trainer,
-					CONCAT(DATE_FORMAT(tc.start_date, '%m/%d/%Y'), ' - ', DATE_FORMAT(tc.end_date, '%m/%d/%Y')) AS dates
-				FROM training_calendar tc
-				JOIN training_enrollment te ON tc.training_id = te.training_id) tec
-				ON (e.first_name = tec.first_name
-					AND e.last_name = tec.last_name
-					AND e.email_address = tec.email_address
-					)
-			WHERE e.employee_nbr LIKE '%".$id."' ";
-			
-			// other parameters added here
-			if (!empty($type)) {
-				$sql .= "AND e.status LIKE '%".$employeeType."%' ";
-			}
-		}
-		
-		// Need to check against SQL injection one day...
-		$result = run_sql($sql);
-		$rows = $result->num_rows;
-		
-		// output data of each row
-		if ($result->num_rows > 0) {
-			while ($row=$result->fetch_assoc()) {
-				$empNbr = $row["employee_nbr"];
-				$firstName = $row["first_name"];
-				$lastName = $row["last_name"];
-				$emailAddress = $row["email_address"];
-				$city = $row["city"];
-				$country = $row["country"];
-				$managerName = $row["manager_name"];
-				$agileTeamName = $row["at_name"];
-				$agileReleaseTrainName = $row["art_name"];
-				$solutionTrainName = $row["st_name"];
-				$role[] = $row["role"];
-				$status[] = $row["status"];
-				$courseName[] = $row["course_name"];
-				$courseCode[] = $row["course_code"];
-				$trainer[] = $row["trainer"];
-				$dates[] = $row["dates"];
-			}
-		}
-		
-		$result->close();	
-		
-	?>
+	<?php 
+		$type = $id = '';
 	
-	<div class="container-fluid buffer">
-		<!-- Employee -->
-		<div class="row">
-			<div class="col-md-9">
-				<table class="table table-condensed table-bordered">
-					<tr>
-						<thead colspan="2" ><h3>Employee</h3></thead>
-					</tr>
-					<tr>
-						<td style="width:200px;">First Name</td>
-						<td><?php echo $firstName ?></td>
-					</tr>
-					<tr>
-						<td>Last Name</td>
-						<td><?php echo $lastName ?></td>
-					</tr>
-					<tr>
-						<td>Email</td>
-						<td><?php echo $emailAddress ?></td>
-					</tr>
-					<tr>
-						<td>City</td>
-						<td><?php echo $city ?></td>
-					</tr>
-					<tr>
-						<td>Country</td>
-						<td><?php echo $country ?></td>
-					</tr>
-					<tr>
-						<td>Manager's Name</td>
-						<td><?php echo $managerName ?></td>
-					</tr>
-				</table>
-			</div>
-		</div>
+		// Get params from url
+		if (isset($_GET["id"])) {
+			$id = strtolower($_GET["id"]);
+		} 
 		
-		<!-- Teams -->
-		<div class="row">
-			<div class="col-md-9">
-				<table class="table table-condensed table-bordered">
-					<tr>
-						<thead colspan="3" ><h3>Teams</h3></thead>
-					</tr>
-					<tr>
-						<th style="width:200px;">Team</th>
-						<th>Team Name</th>
-						<th>Role</th>
-					</tr>
-					
-					<tr>
-						<td>Agile Team</td>
-						<td><?php echo $agileTeamName ?></td>
-						<td><?php displayValues($role) ?></td>
-					</tr>
-					<tr>
-						<td>Agile Release Train</td>
-						<td><?php echo $agileReleaseTrainName ?></td>
-						<td class="disabled"></td>
-					</tr>
-					<tr>
-						<td>Solution Train</td>
-						<td><?php echo $solutionTrainName ?></td>
-						<td></td>
-					</tr>
-				</table>
-			</div>
-		</div>
+		if (isset($_GET["type"])) {
+			$type = strtolower($_GET["type"]);
+		}
+				
+		// Query type check
+		switch($type){
+			case 'emp':
+				emp_query($id);
+				break;
+			case 'at':
+				at_query($id);
+				break;
+			case 'art':
+				art_query($id);
+				break;
+			case 'st':
+				st_query($id);
+				break;
+			default:
+				// Stuff
+				break;
+		}
 		
-		<!-- Training -->
-		<div class="row">
-			<div class="col-md-9">
-				<table class="table table-condensed table-bordered">
-					<tr>
-						<thead colspan="2"><h3>Training</h3></thead>
-					</tr>
-					<tr>
-						<td style="width:200px;">Status</td>
-						<td><?php displayValues($status) ?></td>
-					</tr>
-					<tr>
-						<td>Course Name</td>
-						<td><?php displayValues($courseName) ?></td>
-					</tr>
-					<tr>
-						<td>Course Code</td>
-						<td><?php  displayValues($courseCode) ?></td>
-					</tr>
-					<tr>
-						<td>Trainer</td>
-						<td><?php  displayValues($trainer) ?></td>
-					</tr>
-					<tr>
-						<td>Dates</td>
-						<td><?php  displayValues($dates) ?></td>
-					</tr>
-				</table>
-			</div>
-		</div>
-		
-	</div>
+	?>
+
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/dataTables.bootstrap.min.js"></script>
@@ -310,6 +87,7 @@
 		$(document).ready(function () {
 
 			$('#info').DataTable();
+			$('#info2').DataTable();
 
 		});
 
